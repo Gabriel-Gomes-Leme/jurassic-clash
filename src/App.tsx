e8781818df915720077361d855b2e8aacdc20e5f
Gabriel Gomes <gabrielgl20@outlook.com>
2026-03-22 21:49:50 -0300

    feat: card Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-
-import { faDragon } from '@fortawesome/free-solid-svg-icons'
-import './App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Container } from './components/Container'
-import './styles/global.css'
-import { Cards } from './models/Cards'
+import { faDragon } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Container } from "./components/Container";
+import "./styles/global.css";
+import { Cards } from "./models/Cards";
+import { CardDino } from "./components/CardDino";
 
 function App() {
-
   return (
     <>
       <Container>
-        <h1 className='title-1 text-center'>
+        <h1 className="title-1 text-center mb-4">
           Bem vindo ao Jurassic Clash <FontAwesomeIcon icon={faDragon} />
         </h1>
-        {Cards.map((card) => (
-  <div key={card.id}>
-    {card.name}
-  </div>
-))}
+        <div className="row mt-5">
+          {Cards.map((card) => (
+          <div className="col-12 col-md-4 py-3">
+            <CardDino card={card} />
+          </div>
+        ))}
+        </div>
       </Container>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CardDino/index.tsx
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import type { CardModel } from "../../models/CardModel";
+import styles from "./style.module.css";
+import { faBolt, faShield } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons/faHeart";
+
+type cardProps = {
+  card: CardModel;
+};
+
+const borderColors = {
+  common: "#49c26f",
+  rare: "#41a6e0",
+  epic: '#8452c1',
+  legendary: "#e7be36",
+};
+
+export function CardDino({ card }: cardProps) {
+  return (
+    <div className={styles.cardDino} style={{border: `4px solid ${borderColors[card.rarity]}`}} key={card.id}>
+      <img
+        src={card.image}
+        alt={card.name}
+        title={card.name}
+        className={styles.cardImage}
+      />
+      <div className={styles.cardBody}>
+        <h2 className="card__title">{card.name}</h2>
+        <p className="card__description">{card.description}</p>
+      </div>
+      <div className={styles.cardStats}>
+        <span title="Ataque" className={styles.cardStat} style={{ "--background": "#970000" } as React.CSSProperties}><FontAwesomeIcon
+            aria-label="Ataque"
+            icon={faBolt}
+          ></FontAwesomeIcon> {card.attack}</span>
+        <span title="Vida" className={styles.cardStat} style={{ "--background": "#49c26f" } as React.CSSProperties}>
+          <FontAwesomeIcon
+            aria-label="Vida"
+            icon={faHeart}
+          ></FontAwesomeIcon> {card.hp}</span>
+        <span aria-label="Vida" title="Vida" className={styles.cardStat} style={{ "--background": "#41a6e0" } as React.CSSProperties}>
+          <FontAwesomeIcon
+            aria-label="Defesa"
+            icon={faShield}
+          ></FontAwesomeIcon>
+          {card.defense}
+        </span>
+      </div>
+    </div>
+  );
+}
