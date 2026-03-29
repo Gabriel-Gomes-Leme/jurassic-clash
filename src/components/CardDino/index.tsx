b11092f478991ea1a47158df63cc1060e18a7bb6
Gabriel Gomes <gabrielgl20@outlook.com>
2026-03-29 13:46:10 -0300

    refactor: game styles

--- src/components/CardDino/index.tsx
+++ src/components/CardDino/index.tsx
@@ -6,51 +6,75 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons/faHeart";
 
 type cardProps = {
   card: CardModel;
-  type:"player" | "shop"
+  type: "player" | "shop";
 };
 
 const borderColors = {
   common: "#49c26f",
   rare: "#41a6e0",
-  epic: '#8452c1',
+  epic: "#8452c1",
   legendary: "#e7be36",
 };
 
-const titleSize ={
+const titleSize = {
   player: "0.9rem",
-  shop: "1.2rem"
-}
+  shop: "1.2rem",
+};
 
 export function CardDino({ card, type }: cardProps) {
   return (
-    <div className={styles.cardDino} style={{border: `6px solid ${borderColors[card.rarity]}`}} key={card.id}>
-      <img
-        src={card.image}
-        alt={card.name}
-        title={card.name}
-        className={styles.cardImage}
-      />
-      <div className={styles.cardBody}>
-        <h2 className="card__title fw-bold" style={{color: borderColors[card.rarity], fontSize: titleSize[type]}}>{card.name}</h2>
-        {type === "shop" && <p className="card__description">{card.description}</p>}
-      </div>
-      <div className={styles.cardStats}>
-        <span title="Ataque" className={styles.cardStat} style={{ "--background": "#970000" } as React.CSSProperties}><FontAwesomeIcon
-            aria-label="Ataque"
-            icon={faBolt}
-          ></FontAwesomeIcon> {card.attack}</span>
-        <span title="Vida" className={styles.cardStat} style={{ "--background": "#49c26f" } as React.CSSProperties}>
-          <FontAwesomeIcon
-            aria-label="Vida"
-            icon={faHeart}
-          ></FontAwesomeIcon> {card.hp}</span>
-        <span aria-label="Defesa" title="Defesa" className={styles.cardStat} style={{ "--background": "#41a6e0" } as React.CSSProperties}>
-          <FontAwesomeIcon
+    <div
+      className={styles.cardDino}
+      key={card.id}
+    >
+      <div className={`${styles.cardFace} ${styles.cardFront}`} 
+      style={{ border: `6px solid ${borderColors[card.rarity]}`,
+       backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.5) 51%, rgba(0,0,0,1) 96%), url(${card.image})` }}>
+          <h2
+            className="card__title fw-bold"
+            style={{
+              color: borderColors[card.rarity],
+              fontSize: titleSize[type],
+            }}
+          >
+            {card.name}
+          </h2>
+        <div className={styles.cardStats}>
+          <span
+            title="Ataque"
+            className={styles.cardStat}
+            style={{ "--color": "#af0b0b" } as React.CSSProperties}
+          >
+            <FontAwesomeIcon
+              aria-label="Ataque"
+              icon={faBolt}
+            ></FontAwesomeIcon>{" "}
+            {card.attack}
+          </span>
+          <span
+            title="Vida"
+            className={styles.cardStat}
+            style={{ "--color": "#49c26f" } as React.CSSProperties}
+          >
+            <FontAwesomeIcon aria-label="Vida" icon={faHeart}></FontAwesomeIcon>{" "}
+            {card.hp}
+          </span>
+          <span
             aria-label="Defesa"
-            icon={faShield}
-          ></FontAwesomeIcon>
-          {card.defense}
-        </span>
+            title="Defesa"
+            className={styles.cardStat}
+            style={{ "--color": "#41a6e0" } as React.CSSProperties}
+          >
+            <FontAwesomeIcon
+              aria-label="Defesa"
+              icon={faShield}
+            ></FontAwesomeIcon>
+            {card.defense}
+          </span>
+        </div>
+      </div>
+      <div className={`${styles.cardFace} ${styles.cardBack}`} style={{ border: `6px solid ${borderColors[card.rarity]}` }}>
+        {type === "shop" && <p className="card__description text-center fs-18">{card.description}</p>}
       </div>
     </div>
   );
